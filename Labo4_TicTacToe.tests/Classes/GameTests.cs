05631646add353a2f4ee84626619573c7f4f4944
Vavyo <vavyo2121@gmail.com>
2021-03-27 20:08:53 -0500

    scaffolded tests

---
+++ Labo4_TicTacToe.tests/Classes/GameTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Labo4_TicTacToe.tests.Classes
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Game_given_board_test_for_winners()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Game_switch_player_changes_player_isturn_field()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Confirm_player_position_refers_to_correct_space_on_board()
+        {
+            // arrange
+            // act
+            // assert
+        }
+        [Fact]
+        public void Game_NextPlayer_returns_active_player()
+        {
+            // arrange
+            // act
+            // assert
+        }
+    }
+}
