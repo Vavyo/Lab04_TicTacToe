991dd9ef751f0a9ef3a32cce008896c610060229
Vavyo <vavyo2121@gmail.com>
2021-03-27 20:29:46 -0500

    Test 1 and fixed CheckForWinner() so it works with tests

--- Lab04_TicTacToe/Classes/Game.cs
+++ Lab04_TicTacToe/Classes/Game.cs
@@ -68,7 +68,7 @@ namespace Lab04_TicTacToe.Classes
 		/// </summary>
 		/// <param name="board">current state of the board</param>
 		/// <returns>if winner exists</returns>
-		public bool CheckForWinner(Board board)
+		public static bool CheckForWinner(Board board)
 		{
 			int[][] winners = new int[][]
 			{
@@ -91,9 +91,9 @@ namespace Lab04_TicTacToe.Classes
 				Position p2 = Player.PositionForNumber(winners[i][1]);
 				Position p3 = Player.PositionForNumber(winners[i][2]);
 
-				string a = Board.GameBoard[p1.Row, p1.Column];
-				string b = Board.GameBoard[p2.Row, p2.Column];
-				string c = Board.GameBoard[p3.Row, p3.Column];
+				string a = board.GameBoard[p1.Row, p1.Column];
+				string b = board.GameBoard[p2.Row, p2.Column];
+				string c = board.GameBoard[p3.Row, p3.Column];
 
 				// TODO:  Determine a winner has been reached. 
 				// return true if a winner has been reached. 
--- Labo4_TicTacToe.tests/Classes/GameTests.cs
+++ Labo4_TicTacToe.tests/Classes/GameTests.cs
@@ -12,16 +12,20 @@ namespace Labo4_TicTacToe.tests.Classes
         public void Game_given_board_test_for_winners()
         {
             // arrange
-            
-            // act
-            // assert
+            Board board = new Board();
+
+            // act & assert
+            Assert.False(Game.CheckForWinner(board));
+            board.GameBoard[0, 0] = "O";
+            board.GameBoard[1, 1] = "O";
+            board.GameBoard[2, 2] = "O";
+            Assert.True(Game.CheckForWinner(board));
+
         }
         [Fact]
         public void Game_switch_player_changes_player_isturn_field()
         {
             // arrange
-            // act
-            // assert
         }
         [Fact]
         public void Confirm_player_position_refers_to_correct_space_on_board()
